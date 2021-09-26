53709dc3ea6007ecdda922c87f079925b7d61047
Amy Zhu <ajyzhu@gmail.com>
2021-09-25 18:18:45 -0700

    Testing and fixes

--- src/commands/GenerateChannels.ts
+++ src/commands/GenerateChannels.ts
@@ -1,11 +1,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, GuildMember, Guild, OverwriteData } from 'discord.js';
-import { buildGameState } from '../types/GameState';
-import { buildDefaultPlayerState, PlayerState } from '../types/PlayerState';
 
+// todo: move this into general config
 let categoryName : string = "night-text-channels";
+let gameRole : string = "Current Game";
+let stRole : string = "Storyteller";
 
-// TODO make a get-players utility
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('generatechannels')
@@ -14,11 +14,14 @@ module.exports = {
             .setName('addspectator')
             .setDescription('Make a spectator channel')
             .setRequired(false))
-        .addStringOption(option => option.setName('currentlyPlayingRole')
+        .addStringOption(option => option.setName('currentlyplayingrole')
             .setDescription('Role of current players (default: Current Game)')
             .setRequired(false))
-        .addStringOption(option => option.setName('storytellerRole')
+        .addStringOption(option => option.setName('storytellerrole')
             .setDescription('Role of current storytellers (default: Storyteller)')
+            .setRequired(false))
+        .addStringOption(option => option.setName('categoryname')
+            .setDescription('Name of generated category (default: night-text-channels)')
             .setRequired(false)
         ),
     async execute(interaction: CommandInteraction) {
@@ -32,66 +35,47 @@ module.exports = {
             let guild : Guild = guildOrNull;
             
             const makeSpectator = interaction.options.getBoolean('addspectator');
-            let inGameRoleStr = interaction.options.getString('currentlyPlayingRole') || "Current Game";
-            let storytellerRoleStr = interaction.options.getString('storytellerRole') || "Storyteller";
+            let inGameRoleStr = interaction.options.getString('currentlyplayingrole') || gameRole;
+            let storytellerRoleStr = interaction.options.getString('storytellerrole') || stRole;
+            let categoryNameStr = interaction.options.getString("categoryname") || categoryName;
             
-            // get everyone with a certain role 
-            // TODO bottom scratchings assume nothing in cache
-            // let inGameMembers = guild.roles.fetch()
-            // .then(roles => {
-            //     if (roles) {
-            //         roles.find(role => {
-            //             if (role) {
-            //                 role.name === inGameRole.members()
-            //             }
-            //     })
-            // })
-            // .catch(err => { return interaction.reply('No such role!')});
-
-            // (inGameRole).members.map();
-
-            // let ingameMembers = guild.members.fetch().
-            //     then(members =>
-            //     members.filter(member => member.roles.find("name", inGameRole),
-            //     ))
-
             let inGameRole = guild.roles.cache.find(role => role.name === inGameRoleStr);
-            if (inGameRole === undefined) {
-                return await interaction.reply(`Could not find that role! Role: {$inGameRoleStr}`);
+            console.log(guild.roles.cache.map(role => role.name));
+            if (!inGameRole) {
+                return await interaction.reply(`Could not find that role! Role: ${inGameRoleStr}`);
             }
 
             let inGameMembers = inGameRole.members;
             
             let storytellerRole = guild.roles.cache.find(role => role.name === storytellerRoleStr);
             if (storytellerRole === undefined) {
-                return await interaction.reply(`Could not find that role! Role: {$inGameRoleStr}`);
+                return await interaction.reply(`Could not find that role! Role: ${storytellerRole}`);
             }
             let storytellers = storytellerRole.members;
-
-            
-
-            // make new category
+            // storytellers should be able to use the channels
             let storytellerPermissions: Array<OverwriteData> = storytellers.map(st => { return {id: st, type: "member", allow: "SEND_MESSAGES" }});
-            guild.channels.create(categoryName, {reason: "Category for new night text channels for the current game", type: 'GUILD_CATEGORY'})
+            
+            
+            // make new category
+            guild.channels.create(categoryNameStr, {reason: "Category for new night text channels for the current game", type: 'GUILD_CATEGORY'})
             .then(category => {
+                // make new channels for each member
                 inGameMembers.forEach(member => {
-                    let nickname = member.nickname;
+                    let nickname = member.nickname || member.displayName;
                     let permissions : OverwriteData = {id: member, type: "member", allow: "SEND_MESSAGES"};
                     let disallowed: Array<OverwriteData> = inGameMembers.filter(other => other !== member).map(other => {
                         return {id: other, type: "member", deny: "VIEW_CHANNEL"}
                     });
-                    guild.channels.create(`night-{$nickname}`, { parent: category, type: "GUILD_TEXT", 
-                        permissionOverwrites: [...disallowed, permissions, ...storytellerPermissions]} )
+                    guild.channels.create(`night-${nickname}`, { parent: category, type: "GUILD_TEXT", 
+                        permissionOverwrites: [...disallowed, permissions, ...storytellerPermissions]} );
                 });
                 // TODO also want to make spectator channels that aren't visible to players.
                 // const makeSpectator = interaction.options.getBoolean('addspectator');
 
             })
             .catch(err => console.log("Failure to create new category! " + err));
-            
 
-
-            return await interaction.reply(`Generated channels under {$categoryName}`);
+            return await interaction.reply(`Generated channels under ${categoryName}`);
         } else {
             return await interaction.reply('Must execute this command in a Guild!');
         }
